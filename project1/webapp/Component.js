f699411026eb5aae83e729cf89391fcd7432a1a6
Katherine <katherine@Katherines-MacBook-Pro.local>
2022-02-10 10:35:59 -0600

    working on step 9 component Configuration and got stuck for extended period of time going to step away for a moment

--- project1/webapp/Component.js
+++ project1/webapp/Component.js
@@ -1,19 +1,41 @@
 sap.ui.define([
-        "sap/ui/core/UIComponent",
-        "sap/ui/Device",
-        "project1/model/models"
-    ],
-    function (UIComponent, Device, models) {
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "project1/model/models",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+],
+    function (UIComponent, Device, models, JSONModel, ResourceModel) {
         "use strict";
 
         return UIComponent.extend("project1.Component", {
             metadata: {
-                manifest: "json"
+                "interfaces": ["sap.ui.core.IAsyncContentCreation"],
+                "rootView": {
+                    "viewName": "project1.controller.View1",
+                    "type": "XML",
+                    /*"async": true, // implicitly set via the sap.ui.core.IAsyncContentCreation interface*/
+                    "id": "app"
+                }
             },
 
             init: function () {
                 // call the base component's init function
                 UIComponent.prototype.init.apply(this, arguments);
+                // set data model
+                var oData = {
+                    recipient: {
+                        name: "World"
+                    }
+                };
+                var oModel = new JSONModel(oData);
+                this.setModel(oModel);
+
+                // set i18n model
+                var i18nModel = new ResourceModel({
+                    bundleName: "project1.i18n.i18n"
+                });
+                this.setModel(i18nModel, "i18n");
 
                 // enable routing
                 this.getRouter().initialize();
--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -1,31 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-
+    "sap/m/MessageToast"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast, JSONModel, ResourceModel ) {
+    function (Controller, MessageToast ) {
         "use strict";
 
         return Controller.extend("project1.controller.View1", {
-            onInit: function () {
-                var oData = {
-                    recipient: {
-                        name: "World"
-                    }
-                };
-                var oModel = new JSONModel(oData);
-                this.getView().setModel(oModel);
-                 // set i18n model on view
-                 var i18nModel = new ResourceModel({
-                    bundleName: "project1.i18n.i18n"
-                });
-                this.getView().setModel(i18nModel, "i18n");
-            },
+          
             onShowHello: function () {
                 // read msg from i18n model
                 var oBundle = this.getView().getModel("i18n").getResourceBundle();
